cd592e5696cf1a37cd26f2713190e73e0075b058
unknown <condado.carvalho@gmail.com>
2024-09-05 16:17:57 -0300

    Criação do Projeto

---
+++ src/main/java/com/br/edu/ifsp/bra/aula_git/AulaGitApplication.java
@@ -0,0 +1,13 @@
+package com.br.edu.ifsp.bra.aula_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/edu/ifsp/bra/aula_git/AulaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.edu.ifsp.bra.aula_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
